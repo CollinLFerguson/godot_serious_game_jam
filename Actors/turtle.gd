6d4f6861d7495294445166d6450ce1fd447b6d10
Collin Ferguson <collinleeferguson@gmail.com>
2026-06-20 16:33:09 -0400

    working on turtle boy

--- Actors/turtle.gd
+++ Actors/turtle.gd
@@ -1,9 +1,11 @@
 extends RigidBody2D
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	apply_force(Vector2(1,1))
+	apply_torque(60)
 	pass # Replace with function body.
 
 
@@ -14,8 +16,9 @@ func _process(delta: float) -> void:
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	var velocity := state.get_linear_velocity()
+	var torque := state.get_constant_torque()
 	var step := state.get_step()
 	
-	apply_torque(60)
+	apply_torque(torque)
 	apply_force(velocity)
 	
