a0b91819e209f4276dae2e3fdcf8bf2efda1a942
Collin Ferguson <collinleeferguson@gmail.com>
2026-06-20 18:58:07 -0400

    updates turtle's inertia for maxiumum speeen

--- Actors/turtle.gd
+++ Actors/turtle.gd
@@ -1,32 +1,41 @@
 extends RigidBody2D
 
 @export var base_velocity = Vector2(1,1)
-@export var base_torque = 60
+
 @export var max_velocity = 200
+@export var base_torque = 300
+
+@export var sprite: SpriteFrames = load("res://Actors/Sprites/player.tres")
 
 var items = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	apply_force(base_velocity)
-	apply_torque(base_torque)
-	pass # Replace with function body.
+	$AnimatedSprite2D.sprite_frames = sprite
 
+	apply_central_impulse(base_velocity)
+	apply_torque_impulse(base_torque)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 
+func _physics_process(delta: float) -> void:
+	var velocity = linear_velocity
+	#var rotation_speed = angular_velocity
+	#apply_torque(rotation_speed)
+
+
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	var velocity := state.get_linear_velocity()
-	var torque := state.get_constant_torque()
-	var step := state.get_step()
+	#var torque := state.get_constant_torque()
+	#var step := state.get_step()
 	
-	apply_torque(torque)
 	apply_force(velocity.limit_length(max_velocity))
-	
-	for i in state.get_contact_count():
+
+	# Handle bounce correctly via state
+	for i in range(state.get_contact_count()):
 		var normal = state.get_contact_local_normal(i)
-		linear_velocity = linear_velocity.bounce(normal)
-	
+		var v = state.get_linear_velocity()
+		state.set_linear_velocity(v.bounce(normal))
