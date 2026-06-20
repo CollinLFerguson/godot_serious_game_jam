fe3a636b8954dd7685af2c6a140421c338d6fa93
Jonah Stroup <124182743+jonah-stroup-und@users.noreply.github.com>
2026-06-20 15:17:20 -0500

    Wall
    Collision outline

---
+++ background.gd
@@ -0,0 +1,31 @@
+extends Polygon2D
+
+
+## A thin rectangular map border.
+##
+## This node MUST be a child of a StaticBody2D (or other CollisionObject2D).
+## Set `map_size` and the four-wall outline builds itself; change it at runtime
+## and it rebuilds.
+
+@export var map_size: Vector2 = Vector2(1920, 1080):
+	set(value):
+		map_size = value
+		if is_node_ready():
+			rebuild()
+
+
+func _ready() -> void:
+	rebuild()
+	for x in polygon:
+		print(x)
+	#print(self.polygon.size())
+
+func rebuild() -> void:
+	var w := map_size.x
+	var h := map_size.y
+	polygon = PackedVector2Array([
+		Vector2(0, 0),
+		Vector2(w, 0),
+		Vector2(w, h),
+		Vector2(0, h),
+	])
