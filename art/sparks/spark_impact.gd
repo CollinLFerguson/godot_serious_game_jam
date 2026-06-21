af0097b6f297453f60fc2e926e8c90c063e1842b
JimGringo <66539719+JimGringo@users.noreply.github.com>
2026-06-21 11:16:49 -0500

    Changed how spark impact lives and dies

--- art/sparks/spark_impact.gd
+++ art/sparks/spark_impact.gd
@@ -2,11 +2,16 @@ extends AnimatedSprite2D
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	await get_tree().create_timer(0.5).timeout
-	queue_free()
+#func _ready() -> void:
+	#await get_tree().create_timer(0.5).timeout
+	#queue_free()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_animation_finished():
+	queue_free()
+	#pass
