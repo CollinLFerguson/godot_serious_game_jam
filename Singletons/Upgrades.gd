66ac30e25187a1fc6fa713aff5e9383c2c70556b
AngelinaToste <amtoste9@gmail.com>
2026-06-22 19:55:31 -0400

    attached items to left and right hand, updated item instantiation

--- Singletons/Upgrades.gd
+++ Singletons/Upgrades.gd
@@ -5,42 +5,36 @@ var mace_scene: PackedScene = preload("res://Upgrades/Mace/Mace.tscn")
 
 @onready var mace = get_node("/root/Upgrades/Mace")
 
-var upgrade_dict = {"sword":3, "mace": 2}
+var upgrade_dict = {
+		"sword": {"description" : "It is a sword.", "scene": sword_scene },
+		"mace": {"description" : "It is a mace.", "scene": mace_scene },
+	}
 
-var upgrade_list = ["mace", "mace"]	#can add swords to this to give player more swords
-												#all the logic for this can be moved to the upgrades
-												#tab when ready / someone feels like it
 var actual_upgrades = []
-
 
 func _ready() -> void:
 	SignalBus.upgrade_selected.connect(attachUpgradeItem)
-	
-	
-var upgradeList = [
-		#{"sword" :  , "description": "It is a sword."},
-]
+
+
+func createItemInstantiations(itemName, count):
+	for c in count:
+		actual_upgrades.append(upgrade_dict[itemName]["scene"].instantiate())
 
 '''
 attachUpgradeItem
 attach upgrade to attachment point on turtle
+	var source: node (parent signalling for upgrade attachment)
 	var itemName: string
 '''
 func attachUpgradeItem(source, itemName):
-	upgrade_dict["sword"] += 1
-	print(upgrade_dict["sword"])
-	for x in upgrade_list:
-		if x == "sword":
-			actual_upgrades.append(sword_scene.instantiate())
-		elif x == "mace":
-			actual_upgrades.append(mace_scene.instantiate())
-			#mace.autoplay() #need to determine how to autoplay each instance
-			
-	var upgrade_spawn = source.get_node("Center")
-	var num_items = upgrade_list.count(itemName)
+	var num_items = 2
+	createItemInstantiations(itemName, num_items)
+	
+	var upgrade_spawn_1 = source.get_node("LeftHand")
+	var upgrade_spawn_2 = source.get_node("RightHand")
+	var spawn_points = [upgrade_spawn_1, upgrade_spawn_2]
+	
 	for i in range(num_items):
-		actual_upgrades[i].position = upgrade_spawn.position + Vector2(0,-100)\
-		.rotated((i + 1) * 2 * PI / num_items)
-		actual_upgrades[i].rotation = ((i + 1) *2 * PI) / num_items
+		actual_upgrades[i].position = spawn_points[i].position + Vector2(0,-100)
 		source.add_collision_exception_with(actual_upgrades[i])
 		source.add_child(actual_upgrades[i])
