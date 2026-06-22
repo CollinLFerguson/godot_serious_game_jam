5d927fbc5fcf81dd0d8c44d216f48ff8cfdb75bd
Jonah Stroup <124182743+jonah-stroup-und@users.noreply.github.com>
2026-06-22 15:47:10 -0500

    layer order fixes, single explosion mines, explosion sound fixes

--- art/mine/mine.gd
+++ art/mine/mine.gd
@@ -1,7 +1,7 @@
 extends Area2D
 var targets: Array
 var isarmed = true
-var explode_sound = preload("res://art/explosion/loud-explosion.mp3")
+var explode_sound = preload("res://art/explosion/explosion.mp3")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -17,8 +17,9 @@ func _on_body_entered(body: Node) -> void:
 	if isarmed:
 		#print("blow up")
 		SignalBus.mine_explosion.emit(self, targets)
-		$"../AudioStreamPlayer2D".stream = explode_sound
-		$"../AudioStreamPlayer2D".play()
+		$AudioStreamPlayer2D.stream = explode_sound
+		$AudioStreamPlayer2D.play()
+		self.disable()
 	
 func add_target(object: Node):
 	targets.append(object)
@@ -27,3 +28,11 @@ func add_target(object: Node):
 func remove_target(object: Node):
 	var tgt_loc = targets.find(object)
 	targets.pop_at(tgt_loc)
+	
+func disable():
+	self.set_deferred("monitoring", false)
+	self.set_deferred("monitorable", false)
+	$Mine_AOE.set_deferred("monitoring", false)
+	$Mine_AOE.set_deferred("monitorable", false)
+	$Mine_animation.animation = "explode"
+	isarmed = false
