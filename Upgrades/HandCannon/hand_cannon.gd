d80ad20c00b10849000eb7a2f23a17d7981f730e
AngelinaToste <amtoste9@gmail.com>
2026-06-25 18:33:58 -0400

    cannonball recoil

--- Upgrades/HandCannon/hand_cannon.gd
+++ Upgrades/HandCannon/hand_cannon.gd
@@ -15,11 +15,15 @@ func _on_cannonball_timer_timeout() -> void:
 	var new_ball = cannonballs_scene.instantiate()
 	
 	var turtle_velocity = turtle.linear_velocity
+	var ball_velocity = turtle_velocity * 2
+	#recoil will act on the turtle in 80% of ball velocity in opposite direction
+	var cannon_recoil  = -0.8 * ball_velocity 
 	
-	new_ball.apply_impulse(turtle_velocity * 2)
+	turtle.apply_impulse(cannon_recoil)
+
+	new_ball.apply_impulse(ball_velocity)
 	
 	turtle.add_collision_exception_with(new_ball)
 	add_collision_exception_with(new_ball)
 	get_tree().current_scene.add_child(new_ball)
-
-	new_ball.global_position = self.global_position
+	new_ball.global_position = self.get_node("EndPoint").global_position
