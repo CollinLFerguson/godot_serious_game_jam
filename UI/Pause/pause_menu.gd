acff31f47ad9ffab209ae06d00dccd6c5eeab7ff
Cody <66539719+JimGringo@users.noreply.github.com>
2026-06-26 13:19:25 -0500

    added sliders to adjust volume. Master and music.

---
+++ UI/Pause/pause_menu.gd
@@ -0,0 +1,23 @@
+extends CanvasLayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pause"):
+		if visible:
+			get_tree().paused = false
+			hide()
+		else:
+			get_tree().paused = true
+			show()
+
+func _on_h_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Soundtrack"), value)
+
+
+func _on_master_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
